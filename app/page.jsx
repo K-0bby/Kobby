e22bafdc8e7beffc65312a3ee537b4409bd50180
K-0bby <k.hoenyefia@gmail.com>
2024-08-28 16:59:56 +0000

    update

--- app/page.jsx
+++ app/page.jsx
@@ -10,23 +10,23 @@ export default function Home() {
   return (
     <main className="bg-[#242424] h-screen text-white snap-y snap-mandatory overflow-y-scroll overflow-x-hidden z-0 scroll-smooth scrollbar scrollbar-track-gray-400/2 scrollbar-thumb-accent">
       <Header />
-      <section id="hero" className="snap-start">
+      <section id="hero" className="snap-start h-full">
       <Hero />
       </section>
       
-      <section id="about" className="snap-center">
+      <section id="about" className="snap-center h-full">
       <About />
       </section>
   
-      <section id="skills" className="snap-start">
+      <section id="skills" className="snap-start h-full">
       <Skills />
       </section>
       
-      <section id="projects" className="snap-start">
+      <section id="projects" className="snap-start h-full">
       <Projects />
       </section>
 
-      <section id="contacts" className="snap-start">
+      <section id="contacts" className="snap-start h-full">
       <Contact />
       <Footer />
       </section>
--- components/Project.jsx
+++ components/Project.jsx
@@ -3,27 +3,25 @@ import Link from 'next/link';
 import { CiLink } from 'react-icons/ci';
 import Image from "next/image";
 
-const Project = ({project}) => {
+const Project = ({ project }) => {
   return (
-    <div className="group flex relative">
-        <Image
-            src={project.image}
-            alt={project.alt}
-            width={300}
-            height={200}
-            className="rounded-lg border border-solid border-gray-950 object-cover shadow-lg w-[300px] h-[200px] xl:w-[400px] xl:h-[200px] filter group-hover:accent transition duration-300 ease-in-out"
-        />
-      <div className="absolute opacity-0 group-hover:opacity-80 transition duration-300 ease-in-out group-hover:bg-accent w-[300px] h-[200px]  xl:w-[400px] xl:h-[200px] rounded-lg z-0">
-        <div className="flex flex-col items-center justify-center h-full text-center">
-          <h6 className="text-xl font-bold text-gray-100 opacity-100">{project.title}</h6>
-          <p className="text-gray-100">{project.description}</p>
-          <Link href={project.link} target="_blank" className="text-blue-500 hover:text-blue-700">
-            <CiLink className="text-gray-100 text-[30px]"/>
-          </Link>
-        </div>
+    <div className="group flex relative project-box">
+      <Image
+        src={project.image}
+        alt={project.alt}
+        width={300}
+        height={200}
+        className="rounded-lg object-cover shadow-lg w-[300px] h-[200px] xl:w-[400px] xl:h-[200px] transition-transform duration-500 ease-in-out"
+      />
+      <div className="absolute inset-0 flex flex-col items-center justify-center bg-gradient-to-t from-blue-600 via-transparent to-transparent opacity-0 group-hover:opacity-100 transform translate-y-full group-hover:translate-y-0 transition-all duration-500 ease-in-out project-layer">
+        <h6 className="text-xl font-bold text-white">{project.title}</h6>
+        <p className="text-white">{project.description}</p>
+        <Link href={project.link} target="_blank">
+          <CiLink className="text-white text-[30px]" />
+        </Link>
       </div>
     </div>
-  )
+  );
 }
 
-export default Project
+export default Project;
