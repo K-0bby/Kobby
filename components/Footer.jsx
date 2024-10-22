d5d94fe055f0c4fb000ad6dea5aa41776b4b0ea6
K-0bby <k.hoenyefia@gmail.com>
2024-10-22 16:48:53 +0000

    update

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -8,13 +8,13 @@ const Footer = () => {
       <div className="container mx-auto flex flex-col items-center justify-center space-y-4">
         {/* Social Media Icons */}
         <div className="flex space-x-6">
-          <Link href="https://github.com/yourusername" target="_blank" rel="noopener noreferrer" aria-label="GitHub">
+          <Link href="https://github.com/K-0bby" target="_blank" rel="noopener noreferrer" aria-label="GitHub">
             <FaGithub className="text-xl hover:text-accent transition duration-300" />
           </Link>
-          <Link href="https://linkedin.com/in/yourusername" target="_blank" rel="noopener noreferrer" aria-label="LinkedIn">
+          <Link href="https://www.linkedin.com/in/gideon-hoenyefia-74a93a233/" target="_blank" rel="noopener noreferrer" aria-label="LinkedIn">
             <FaLinkedin className="text-xl hover:text-accent transition duration-300" />
           </Link>
-          <Link href="https://twitter.com/yourusername" target="_blank" rel="noopener noreferrer" aria-label="Twitter">
+          <Link href="https://x.com/HoenyefiaGideon" target="_blank" rel="noopener noreferrer" aria-label="Twitter">
             <FaTwitter className="text-xl hover:text-accent transition duration-300" />
           </Link>
         </div>
