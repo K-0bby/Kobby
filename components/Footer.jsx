73a4e77ab916316a203378177e1aa96caa86d0af
K-0bby <k.hoenyefia@gmail.com>
2025-01-03 00:02:34 +0000

    update

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,6 +1,6 @@
-import React from 'react';
-import Link from 'next/link';
-import { FaGithub, FaLinkedin, FaTwitter } from 'react-icons/fa';
+import React from "react";
+import Link from "next/link";
+import { FaGithub, FaLinkedin, FaTwitter } from "react-icons/fa";
 
 const Footer = () => {
   return (
@@ -8,24 +8,40 @@ const Footer = () => {
       <div className="container mx-auto flex flex-col items-center justify-center space-y-4">
         {/* Social Media Icons */}
         <div className="flex space-x-6">
-          <Link href="https://github.com/K-0bby" target="_blank" rel="noopener noreferrer" aria-label="GitHub">
+          <Link
+            href="https://github.com/K-0bby"
+            target="_blank"
+            rel="noopener noreferrer"
+            aria-label="GitHub"
+          >
             <FaGithub className="text-xl hover:text-accent transition duration-300" />
           </Link>
-          <Link href="https://www.linkedin.com/in/gideon-hoenyefia-74a93a233/" target="_blank" rel="noopener noreferrer" aria-label="LinkedIn">
+          <Link
+            href="https://www.linkedin.com/in/gideon-hoenyefia-74a93a233/"
+            target="_blank"
+            rel="noopener noreferrer"
+            aria-label="LinkedIn"
+          >
             <FaLinkedin className="text-xl hover:text-accent transition duration-300" />
           </Link>
-          <Link href="https://x.com/HoenyefiaGideon" target="_blank" rel="noopener noreferrer" aria-label="Twitter">
+          <Link
+            href="https://x.com/HoenyefiaGideon"
+            target="_blank"
+            rel="noopener noreferrer"
+            aria-label="Twitter"
+          >
             <FaTwitter className="text-xl hover:text-accent transition duration-300" />
           </Link>
         </div>
 
         {/* Copyright Text */}
         <p className="text-sm tracking-wider text-center mx-auto text-gray-400">
-          Copyright &copy; {new Date().getFullYear()} by Gideon Korbla Hoenyefia | All Rights Reserved
+          Copyright &copy; {new Date().getFullYear()} by Gideon Korbla Hoenyefia
+          | All Rights Reserved
         </p>
       </div>
     </footer>
   );
-}
+};
 
 export default Footer;
